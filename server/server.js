1212d46244e384593abae3985977a10493716d03
Chris Kuh <kuh_chris@yahoo.com>
2016-06-23 16:32:39 -0700

    [feature] setup server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+module.exports.app = app;
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name');
+  next();
+});
+
+app.set('port', process.env.PORT || 1337);
+
+app.use(express.static('./client'));
+
+if (module.parent) {
+  module.exports = app;
+} else {
+  app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+}
